b14f852ddff95662d78e243fd173e9d33c54bf3f
Indy Iri <indy.iri@student.pxl.be>
2024-12-12 10:46:46 +0100

    Afgewerkt

--- H12Oef1Namenlijst/H12Oef1Namenlijst/Program.cs
+++ H12Oef1Namenlijst/H12Oef1Namenlijst/Program.cs
@@ -1,1 +1,22 @@
 ﻿
+
+string[] names = {"Eli","Indy", "Ingrid","Nuri","Dori","Chum", "Simba", "Jamba", "Mama", "Papa", "Iri" };
+
+Console.WriteLine("=== Namen afdrukken ===");
+Console.WriteLine();
+for (int i = 0; i < names.Length; i++)
+{
+    Console.WriteLine($"Naam {i+1}: {names[i]}");
+}
+
+Console.WriteLine();
+Console.WriteLine();
+Console.WriteLine();
+
+Console.WriteLine("=== Namen gesorteerd afdrukken ===");
+Console.WriteLine();
+Array.Sort(names);
+for (int i = 0; i < names.Length; i++)
+{
+    Console.WriteLine($"Naam {i + 1}: {names[i]}");
+}
